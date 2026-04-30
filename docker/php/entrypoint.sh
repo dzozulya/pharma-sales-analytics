10d7a196b95a42e287b7152ee5fc947264ab6157
agent3030 <agent3030@gmail.com>
2026-04-30 14:43:02 +0300

    fix permissions

--- docker/php/entrypoint.sh
+++ docker/php/entrypoint.sh
@@ -3,6 +3,6 @@ set -e
 
 mkdir -p /app/runtime/cache /app/runtime/logs /app/web/assets
 
-chmod -R 775 /app/runtime /app/web/assets /app/storage || true
+chmod -R 777 /app/runtime /app/web/assets  /app/storage/imports|| true
 
 exec "$@"
